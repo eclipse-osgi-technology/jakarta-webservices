a3fee8263207864d0387c3ee5f206a5a5e638a82
Christoph Läubrich <laeubi@laeubi-soft.de>
2025-05-13 16:28:55 +0200

    Print infos about registered handlers and order

--- runtime/registrar/src/main/java/org/eclipse/osgi/technology/webservices/registrar/EndpointRegistrar.java
+++ runtime/registrar/src/main/java/org/eclipse/osgi/technology/webservices/registrar/EndpointRegistrar.java
@@ -178,7 +178,7 @@ public class EndpointRegistrar implements WebserviceServiceRuntime {
      */
     @Reference(cardinality = ReferenceCardinality.MULTIPLE, policy = ReferencePolicy.DYNAMIC)
     public void addHandler(ServiceReference<Handler<? extends MessageContext>> handler) {
-        logger.debug("UPDATE handler={}", handler);
+		logger.debug("ADD handler={}", handler);
         HandlerInfo info = handlerMap.put(handler, new HandlerInfo(handler, context.getBundleContext()));
         if (info != null) {
             info.dispose();
